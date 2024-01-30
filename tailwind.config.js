69edb080caa4cc065509cd1c71a2badcbbeb45da
nmoskaleva <natalia.moskaleva@criipto.com>
2024-01-30 16:21:39 +0100

    Set up Vite & Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    colors: {
+      primary600: '#604FED', // Primary bright purple
+      lightBlue900: '#262E52',
+      lightBlue800: '#424974',
+      lightBlue300: '#BABCD7',
+      lightBlue25: '#F7F7FF',
+      white: '#FFFFFF',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
